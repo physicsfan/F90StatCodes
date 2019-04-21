4496dff4e1101fe6ddb2c54b36a95e3ded426dc4
physicsfan <asenchuk@hotmail.com>
2019-04-20 22:21:15 -0500

    changes to factorial.f90

--- factorial.f90
+++ factorial.f90
@@ -1,31 +1,33 @@
-module my_name_is_mr_fibbs
-	contains
-
-    recursive integer function factorial(x) result(answer)
+module show_me_the_facs
+contains
+  
+  recursive integer function factorial(x) result(answer)
     implicit none
     integer, intent(in) :: x
     
-	if(x == 0) then
-      answer = 1
+    if(x == 0) then
+       answer = 1
     else
-      answer = x * factorial(x-1)
+       answer = x * factorial(x-1)
     end if
-    end function factorial
-end module
+  end function factorial
+  
+end module show_me_the_facs
 
 program just_the_facs
-  use my_name_is_mr_fibbs
+  use show_me_the_facs
+  ! program to test the recursive factorial function 
   implicit none
-
+  
   integer :: n
-
+  
   print '(A)', "Please enter an integer:"
   read(*,*) n
- 
+  
   if(n > 9) then
-	print '(A)', "n is TOO BIG!"
+     print '(A)', "n is TOO BIG!"      ! test overflow of integers
   else  
-  	print '(/,i2,A,i6)', n, "! is: ", factorial(n)
+     print '(/,i2,A,i6)', n, "! is: ", factorial(n)
   endif
-
-  end
+  
+end program just_the_facs
