237f2065c9a28945ef80ba35a807417d464d718d
physicsfan <asenchuk@hotmail.com>
2019-04-23 22:11:35 -0500

    Improved formatting

--- suffix.f90
+++ suffix.f90
@@ -1,55 +1,60 @@
-module suffix
-
-	contains
-
-    integer function add_suffix(file_name, suffix) result(answer)
-    	! Returns 0 if operation is successful, 1 otherwise.
-        implicit none
-        character(len=*), intent(inout) :: file_name
-        character(len=3), intent(in) :: suffix
-        integer :: i
-        
-		if(suffix(1:1)==::) goto 5				! suffix begins with blank
-        i = index(file_name, ".", back=.true.)  ! look for last period
-        if(i==0) i = len_trim(file_name) + 1	! if no period was found
-        if(len(file_name) < i+3) goto 5			! not enough room for suffix
-        
-		file_name(i:) = "." // suffix	! add suffix to file name
-        
-		! normal exit
-        answer = 0
-        return
-        ! error trap
-    5   answer = 1
-     	return       
-	end function add_suffix
-
-    integer function fuck_suffix(file_name) result(answer)
-    	! Adds a .fuck suffix to any filename
-    	! Returns 0 if operation is successful, 1 otherwise.
-        implicit none
-        character(len=*), intent(inout) :: file_name
-        character(len=4), intent(out) :: suffix = "fuck"
-        integer :: i
-        
-		if(suffix(1:1)==::) goto 5				! suffix begins with blank
-        i = index(file_name, ".", back=.true.)  ! look for last period
-        if(i==0) i = len_trim(file_name) + 1	! if no period was found
-        if(len(file_name) < i+3) goto 5			! not enough room for suffix
-        
-		file_name(i:) = "." // suffix	! add suffix to file name
-        
-		! normal exit
-        answer = 0
-        return
-        ! error trap
-    5   answer = 1
-     	return       
-	end function fuck_suffix
-
-end module suffix
+module suffix_types
+  
+contains
+  
+  integer function add_suffix(file_name, suffix) result(answer)
+    ! Returns 0 if operation is successful, 1 otherwise.
+    implicit none
+    character(len=*), intent(inout) :: file_name
+    character(len=3), intent(in) :: suffix
+    ! declare locals
+    integer :: i
+    
+    if(suffix(1:1)=="") goto 5				! suffix begins with blank
+    i = index(file_name, ".", back=.true.)              ! look for last period
+    if(i==0) i = len_trim(file_name) + 1  	        ! if no period was found
+    if(len(file_name) < i+3) goto 5			! not enough room for suffix
+    
+    file_name(i:) = "." // suffix	! add suffix to file name
+    
+    ! normal exit
+    answer = 0
+    return
+    ! error trap
+5   answer = 1
+    return       
+  end function add_suffix
+  
+end module suffix_types
 
 program test_suffix
-use suffix
-! a main program to test the add_suffix function
-implicit none
+  use suffix_types
+  ! a main program to test the add_suffix function
+  implicit none
+  
+  integer, parameter :: fileLengthMax = 80
+  integer, parameter :: suffixLength = 4
+  character(len=fileLengthMax) :: fileName = ""
+  character(len=suffixLength) :: suffix = ""
+  
+  ! section if multiple file-names are sought
+  !integer :: number_of_filenames
+  !character(len=fileLengthMax), allocatable :: fileNames(:)
+  
+  !print "(A)", "Please enter the number of filenames wanted:"
+  !read(*,*) number_of_filenames
+  
+  !allocate(fileNames(number_of_filenames))
+  
+  
+  print "(A)", "Please enter the name of the file and its desired suffix:"
+  read(*,*) fileName, suffix
+  
+  if (add_suffix(fileName, suffix) == 0) then
+     print "(A)", "Success!"
+     print "(A,A)", "Your new file name is: ",fileName
+  else
+     print "(A)", "There was an error"
+  endif
+  
+end program test_suffix
