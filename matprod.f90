882ced0cb13c284155620a9a896f7dd271b80309
physicsfan <asenchuk@hotmail.com>
2019-04-30 21:19:08 -0500

    New versions of mat_prod and uniform

--- matprod.f90
+++ matprod.f90
@@ -1,77 +1,145 @@
 module mat_stuff
 
-contains
-  
-  subroutine matprod(mat1, mat2, mprod, error_code)
-    implicit none
-    integer, intent(out) :: error_code
+	contains
+
+    integer function mat_prod(mat1, mat2, mprod) result(error_code)
+    ! function that calculate the matrix product of input matrices.
+    ! function checks to see if the sizes of the inputs are correct.
+	implicit none
+    
+	! declare variables
+    integer :: error_code
     real, intent(in) :: mat1(:,:), mat2(:,:)
     real, intent(out) :: mprod(:,:)
+	
+	! check for matrix conformity
+    ! Note: goto only used for error traps
+	if(size(mat1,2) /= size(mat2,1)) goto 1
     
-    ! check for matrix conformity
-    if(size(mat1,2) /= size(mat2,1)) then
-       mprod=0.0
-       error_code = 1
-    else
-       mprod = matmul(mat1, mat2)
-       error_code = 0
-    endif
+	! normal return
+	mprod = matmul(mat1, mat2)
+    error_code = 0
+    return    	
+	
+	! error trap
+1	continue    
+	mprod=0.0
+    error_code = 1
+    return
     
-  end subroutine matprod
-  
-  subroutine fill_with_uniforms(mat, lower, upper)
+	end function mat_prod
+
+
+    subroutine init_random_seed()
+    ! subroutine to initialize the random number generator
+    
+	! declare locals
+	integer :: i, n, clock
+	integer, dimension(:), allocatable :: seed
+	
+	! initialize random seed with system clock
+	call random_seed(size = n)
+	allocate(seed(n))
+	call system_clock(count=clock)
+	seed = clock + 37 * (/ (i - 1, i = 1, n) /)
+	call random_seed(PUT = seed)
+	
+	deallocate(seed)
+	
+	end subroutine init_random_seed
+
+
+    subroutine fill_with_uniforms(mat, lower, upper)
     ! Fills the rank_one array vec with random numbers uniformly
     ! distributed from lower to upper, which default to 0.0 and
     ! 1.0, respectively
     implicit none
-    ! declare arguments
+    
+	! declare arguments
     real, intent(out) :: mat(:,:)
     real, intent(in), optional :: lower, upper
-    ! declare locals
+    
+	! declare locals
     real :: a, b
-    ! begin
+    
+	! begin
     a = 0.0
     b = 1.0
     if(present(lower)) a = lower
     if(present(upper)) b = upper
     call random_number(mat)
     mat = a + mat * (b - a)
-  end subroutine fill_with_uniforms
-  
+    
+	end subroutine fill_with_uniforms
+
 end module mat_stuff
 
 
 program mat_test
+  ! Main program to test the mat_prod function and other elements of the 
+  ! mat_stuff module.
   use mat_stuff
   implicit none
-  integer :: i, j, rows1, columns1, rows2, columns2
+  
+  ! declare locals
+  integer :: i, j, xrows, xcolumns, yrows, ycolumns
   real :: a, b
-  real, allocatable :: x(:,:), y(:,:)
+  character(len=3) :: boundsWanted
+  real, allocatable :: x(:,:), y(:,:), matProd(:,:)
   
-  print "(A)", "Enter the lower an upper bounds:"
-  read(*,*) a, b
+  call init_random_seed()	! initialize random number generator
+  
+  ! obtain sizes of input matrices
   print "(A)", "How many rows and columns do you want for x? (max 100)"
-  read(*,*) rows1, columns1
+  read(*,*) xrows, xcolumns
   print "(A)", "How many rows and columns do you want for y? (max 100)"
-  read(*,*) rows2, columns2  
+  read(*,*) yrows, ycolumns  
   
-  allocate(x(rows1,columns1))
-  allocate(y(rows2,columns2))
+  ! allocate all matrices
+  allocate(x(xrows,xcolumns))
+  allocate(y(yrows,ycolumns))
+  allocate(matProd(xrows,ycolumns))
+
+  ! select if user wants numbers between a particular range
+  print '(A)', "Do you want explicit lower and upper bounds?"
+  read(*,*) boundsWanted
   
-  call fill_with_uniforms(x, upper=b, lower=a)
-  call fill_with_uniforms(y, upper=b, lower=a)
+  ! fill input matrices with values using a random number generator
+  if((boundsWanted(1:1) == 'y') .or. (boundsWanted(1:1) == 'Y')) then
+    print "(A)", "Enter the lower an upper bounds:"
+  	read(*,*) a, b
+    call fill_with_uniforms(x, upper=b, lower=a)
+    call fill_with_uniforms(y, upper=b, lower=a)
+  else
+    call fill_with_uniforms(x)
+    call fill_with_uniforms(y)
+  endif
   
-  
+  ! print out input matrices, X and Y
   print "(/,A)", "Here is x:"
-  do i = 1, rows1
-     print '(100f8.3)', (x(i,j), j = 1, columns1) 
+  do i = 1, xrows
+   print '(100f8.3)', (x(i,j), j = 1, xcolumns) 
   end do
-  
   print "(/,A)", "Here is y:"
-  do i = 1, rows2
-     print '(100f8.3)', (y(i,j), j = 1, columns2) 
+  do i = 1, yrows
+   print '(100f8.3)', (y(i,j), j = 1, ycolumns) 
   end do
   
-  deallocate(x,y)   ! not really necessary
+  ! calculate matrix product, return error if input matrices do not
+  ! have the correct bounds
+  select case(mat_prod(x, y, matProd))
+  case(1)
+	print '(/,A,/)', "Error: The input matrix sizes do not conform..."
+  	do i = 1, size(matProd,1)
+   		print '(100f8.3)', (matProd(i,j), j = 1, size(matProd,2)) 
+  	end do
+  case default
+	print '(/,A,/)', "Success: The product of x and y is..."
+  	do i = 1, size(matProd,1)
+   		print '(100f8.3)', (matProd(i,j), j = 1, size(matProd,2)) 
+  	end do	
+  end select
   
+  deallocate(x,y,matProd)   ! not really necessary
+
 end program mat_test
--- uniform.f90
+++ uniform.f90
@@ -2,57 +2,65 @@
 module randomN
       ! a simple module for generating randum numbers
 contains
-  subroutine init_random_seed()
-    integer :: i, n, clock
-    integer, dimension(:), allocatable :: seed
-    
-    call random_seed(size = n)
-    allocate(seed(n))
-    
-    call system_clock(count=clock)
-    
-    seed = clock + 37 * (/ (i - 1, i = 1, n) /)
-    call random_seed(PUT = seed)
-    
-    deallocate(seed)
-  end subroutine init_random_seed
-  
-  subroutine fill_with_uniforms(vec, lower, upper)
-    ! Fills the rank_one array vec with random numbers uniformly
-    ! distributed from lower to upper, which default to 0.0 and
-    ! 1.0, respectively
-    implicit none
-    ! declare arguments
-    real, intent(out) :: vec(:)
-    real, intent(in), optional :: lower, upper
-    ! declare locals
-    real :: a, b
-    ! begin
-    a = 0.0
-    b = 1.0
-    if(present(lower)) a = lower
-    if(present(upper)) b = upper
-    call random_number(vec)
-    vec = a + vec * (b - a)
-  end subroutine fill_with_uniforms
+	  subroutine init_random_seed()
+			integer :: i, n, clock
+			integer, dimension(:), allocatable :: seed
+
+			call random_seed(size = n)
+			allocate(seed(n))
+
+			call system_clock(count=clock)
+
+			seed = clock + 37 * (/ (i - 1, i = 1, n) /)
+			call random_seed(PUT = seed)
+
+			deallocate(seed)
+	  end subroutine init_random_seed
+      
+	  subroutine fill_with_uniforms(vec, lower, upper)
+            ! Fills the rank_one array vec with random numbers uniformly
+            ! distributed from lower to upper, which default to 0.0 and
+            ! 1.0, respectively
+            implicit none
+            ! declare arguments
+            real, intent(out) :: vec(:)
+            real, intent(in), optional :: lower, upper
+            ! declare locals
+            real :: a, b
+            ! begin
+            a = 0.0
+            b = 1.0
+            if(present(lower)) a = lower
+            if(present(upper)) b = upper
+            call random_number(vec)
+            vec = a + vec * (b - a)
+      end subroutine fill_with_uniforms
 end module randomN
 !######################################################################
 program uniform
   ! Generates random numbers uniformly distributed between a and b
-  use randomN      ! allows the program to use fill_with_uniforms
+  use randomN      			! allows the program to use fill_with_uniforms
   implicit none
+
+  ! declare variables
   integer :: i, n
   real :: a, b
   real, allocatable :: x(:)
+  call init_random_seed()	! initialize random number generator
+
+  
   print "(A)", "Enter the lower an upper bounds:"
   read(*,*) a, b
   print "(A)", "How many random numbers do you want?"
   read(*,*) n
   allocate(x(n))
+  
   call fill_with_uniforms(x, upper=b, lower=a)
+  
   print "(A)", "Here they are:"
   do i = 1, n
-     print *, x(i)
+      print *, x(i)
   end do
   deallocate(x)   ! not really necessary
+
 end program uniform
